a280c69106188a6c19cc4dd93e2b069305c27dcc
Miroslav Gajic <gajic.miroslav@gmail.com>
2017-03-03 17:36:31 +0100

    Adding a new question, showing questions in a list

---
+++ app/directives/questions-config.js
@@ -0,0 +1,19 @@
+﻿app.directive('questionsConfig', function () {
+    
+    return {
+        restrict: 'E',
+        templateUrl: getTemplatePath('questions-config'),
+        scope:true
+    };
+});
+
+app.controller('QuestionsConfigCtrl', ['$scope', function ($scope) {
+
+    $scope.questions = [];
+    $scope.newQuestion = 'TEST';
+
+    $scope.addQuestion = function () {
+
+        $scope.questions.push($scope.newQuestion);
+    };
+}]);
